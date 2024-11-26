263393b052af569d39470535a84d0d36c983d8d1
beau <a02278030@aggies.usu.edu>
2024-11-26 00:42:33 -0700

    working game theory and involvoing other agents in moves

--- agents.py
+++ agents.py
@@ -1,5 +1,6 @@
 from mesa import Agent
 import numpy as np
+import random
 
 
 class EvacuationAgent(Agent):
@@ -42,27 +43,63 @@ class EvacuationAgent(Agent):
             self.model.grid.remove_agent(self)
             return
 
-        neighbors = self.model.grid.get_neighborhood(
+        neighborhood = self.model.grid.get_neighborhood(
             self.pos, moore=True, radius=self.model.radius
         )
-        best_pos = self.pos
 
-        #! you might want to put a loop here just in case another agent steals this agent move
-        # find empty space that moves toward your closest exit
-        for i in neighbors:
-            if self.model.grid.is_cell_empty(i):
-                if (np.linalg.norm(np.array(best_pos) - np.array(self.closest_exit)) > (np.linalg.norm(np.array(i) - np.array(self.closest_exit)))):
+        #! you might want to put a loop here just in case another agent steals this agents move
+        potential_move = True # default check variable
+        while potential_move:
+            best_pos = self.pos
+
+            # find empty space that moves toward your closest exit
+            potential_move = False
+            for i in neighborhood:
+                if self.model.grid.is_cell_empty(i) and (np.linalg.norm(np.array(best_pos) - np.array(self.closest_exit)) > (np.linalg.norm(np.array(i) - np.array(self.closest_exit)))):
                     best_pos = i
+                    potential_move = True
+
+            if best_pos == self.pos: # check if have new pos
+                return
 
 
-        #! see if any agents want the exit and haven't already moved this round
-        #! Chance of moving based on neighbors maybe decide what random number it will be for each overall step in model
+            # see if any agents want the exit and haven't already moved this round
+            neighbors = self.model.grid.get_neighbors(
+            self.pos, moore=True, include_center=True, radius=1
+            )
+            potential_movers = []
+            for i in neighbors:
+                if (not i.moved) and (np.linalg.norm(np.array(best_pos) - np.array(i.closest_exit)) < (np.linalg.norm(np.array(i.pos) - np.array(i.closest_exit)))): #! maybe find their own current best step in a method would add more complexity
+                    potential_movers.append(i)
 
-        if best_pos != self.pos: # check if have new pos
-            self.model.grid.move_agent(self, best_pos)
-            self.moved = True
-        self.find_nearest_exit() # reevaluate nearest exit
 
+            # Chance of moving based on neighbors
+            defectors = [i for i in potential_movers if i.move == "D"]
+            cooperators = [i for i in potential_movers if i.move == "C"]
+            if len(defectors) == 1: # only 1 defector
+                winner = defectors[0]
+            elif len(cooperators) == len(potential_movers) or len(potential_movers) == 1: # all coop
+                chance = len(potential_movers)
+                r_int = random.randint(0,  chance - 1)
+                winner = potential_movers[r_int]
+            else: # contain multiple deflectors
+                chance = len(defectors) * 2 # 2 is penalty but can create variable alter
+                r_int = random.randint(0, chance - 1)
+                if r_int < len(defectors):
+                    winner = defectors[r_int]
+                else: # no one moves!
+                    return
+
+
+            # moving and updating status for the winning agent
+            self.model.grid.move_agent(winner, best_pos)
+            winner.moved = True
+            winner.find_nearest_exit() # reevaluate nearest exit
+
+            if self.moved:
+                return
+
+            #! update status of cooperators that lost the opportunity to win the spot
 
     def reset_moved(self) -> None:
         self.moved = False
@@ -74,3 +111,4 @@ class EvacuationAgent(Agent):
             if (new_distance < distance):
                 distance = new_distance
                 self.closest_exit = exit_pos
+
